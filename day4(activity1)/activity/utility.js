4b726aa2b8541393378b549e3df46d6f131107be
pranjal <aryapranjal1999@gmail.com>
2020-06-06 10:50:27 +0530

    commit2

--- day4(activity1)/activity/utility.js
+++ day4(activity1)/activity/utility.js
@@ -2,6 +2,8 @@ module.exports.types={
     media: ["mp4","mkv"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    images: ['img','jpg'],
+    audio: ["amr",'mp3'],
     app:['exe','pkg','deb']
 }
 //for (let category in types) {
