f81cc3e89697e41bff53155e6bd94166f8444178
pranjal <aryapranjal1999@gmail.com>
2020-06-10 16:09:14 +0530

    next change

---
+++ day7/notice_board/emailLogic.js
@@ -0,0 +1,22 @@
+"use strict";
+const nodemailer = require("nodemailer");
+// async..await is not allowed in global scope, must use a wrapper
+module.exports.sendEmail = async function (html) {
+  // settings generate 
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "aryapranjal1999@gmail.com", // generated ethereal user
+      pass: YOUR_APP_PASSWORD // generated ethereal password
+    },
+  });
+  // send mail with defined transport object
+  await transporter.sendMail({
+    from: '"PRANJAL 👻" <aryapranjal1999@gmail.com>', // sender address
+    to: "aryapranjal1999@gmail.com", // list of receivers
+    subject: "Notice",
+    html: html, // html body
+  });
+}
